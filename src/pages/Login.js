7a015ac343ed5ef015475fe380535c0abe250166
KHAIRUL ISLAM <khairulislam38@gmail.com>
2022-09-29 00:38:35 +0600

    update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -64,7 +64,7 @@ const Login = () => {
                   autoComplete="email"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-                  placeholder="Email address"
+                  placeholder="Email address/(test@g.com)"
                 />
               </div>
               <div>
@@ -80,7 +80,7 @@ const Login = () => {
                   autoComplete="current-password"
                   required
                   className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-violet-500 focus:border-violet-500 focus:z-10 sm:text-sm"
-                  placeholder="Password"
+                  placeholder="Password/(1234)"
                 />
               </div>
             </div>
